33a061233d7b42164309af271250e7af64390ee7
杨光荣 <330210073@qq.com>
2018-04-18 11:20:00 +0800

    security-feature-6-06  原始分支

---
+++ imooc-security-core/src/main/java/com/imooc/security/core/social/ImoocSpringSocialConfigurer.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.imooc.security.core.social;
+
+import org.springframework.social.security.SocialAuthenticationFilter;
+import org.springframework.social.security.SpringSocialConfigurer;
+
+/**
+ * @author zhailiang
+ *
+ */
+public class ImoocSpringSocialConfigurer extends SpringSocialConfigurer {
+
+	private String filterProcessesUrl;
+
+	private SocialAuthenticationFilterPostProcessor socialAuthenticationFilterPostProcessor;
+
+	public ImoocSpringSocialConfigurer(String filterProcessesUrl) {
+		this.filterProcessesUrl = filterProcessesUrl;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	protected <T> T postProcess(T object) {
+		SocialAuthenticationFilter filter = (SocialAuthenticationFilter) super.postProcess(object);
+		filter.setFilterProcessesUrl(filterProcessesUrl);
+		if (socialAuthenticationFilterPostProcessor != null) {
+			socialAuthenticationFilterPostProcessor.process(filter);
+		}
+		return (T) filter;
+	}
+
+	public String getFilterProcessesUrl() {
+		return filterProcessesUrl;
+	}
+
+	public void setFilterProcessesUrl(String filterProcessesUrl) {
+		this.filterProcessesUrl = filterProcessesUrl;
+	}
+
+	public SocialAuthenticationFilterPostProcessor getSocialAuthenticationFilterPostProcessor() {
+		return socialAuthenticationFilterPostProcessor;
+	}
+
+	public void setSocialAuthenticationFilterPostProcessor(
+			SocialAuthenticationFilterPostProcessor socialAuthenticationFilterPostProcessor) {
+		this.socialAuthenticationFilterPostProcessor = socialAuthenticationFilterPostProcessor;
+	}
+
+}
